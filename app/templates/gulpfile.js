98f55301725381a6e89e3efe19a75957f47f6631
Gabriel Florit <gabrielflorit@gmail.com>
2014-03-21 15:54:01 -0400

    compile-sass-all should compile both scss and css

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -172,7 +172,7 @@ gulp.task('build-html-prod', function() {
 });
 
 gulp.task('compile-sass-all', function() {
-	return compileSass('css/*.scss');
+	return compileSass('css/*');
 });
 
 function compileSass(path) {
