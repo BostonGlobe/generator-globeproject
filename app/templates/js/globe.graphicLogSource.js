ace447559b91a5f3bae28f33f6bfebf258cec66c
Gabriel Florit <gabrielflorit@gmail.com>
2014-04-07 15:04:32 -0400

    added graphicLogSource

---
+++ app/templates/js/globe.graphicLogSource.js
@@ -0,0 +1,27 @@
+globe.graphicLogSource = function(url) {
+	if (window.console && console.log) {
+		console.log('      ########                ##        ');
+		console.log('     ##########              ####       ');
+		console.log('  ################       #  #######     ');
+		console.log(' #            ##       #      ######    ');
+		console.log('##           #       #        #######   ');
+		console.log('           ###    ####          ####### ');
+		console.log('         #####   #####          ########');
+		console.log('        ####    ######            ####  ');
+		console.log('        ####    ######            ##    ');
+		console.log('        ####    ######        ####      ');
+		console.log('############    ######      ########    ');
+		console.log('##    ######    ######      ############');
+		console.log('        ####    ######          ########');
+		console.log('        ####    ####            ########');
+		console.log('      #####     #               ########');
+		console.log('         #    #                 ########');
+		console.log('          ####                  ########');
+		console.log('    ##################          ####### ');
+		console.log('  ###          #########        #       ');
+		console.log('  #                #########  #         ');
+		console.log('##                      ###             ');
+		console.log('  ##');
+		console.log('Source code for this project at ' + url + ' .');
+	}
+};
