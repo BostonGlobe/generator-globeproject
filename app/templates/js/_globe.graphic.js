ffc3e59e7ac2194e3b9f2fd9589edea030ccc776
Gabriel Florit <gabrielflorit@gmail.com>
2014-07-03 18:39:43 -0400

    Update _globe.graphic.js

--- app/templates/js/_globe.graphic.js
+++ app/templates/js/_globe.graphic.js
@@ -1,11 +1,9 @@
 globe.graphic = function() {
-
 	<% if (includeMobileTemplate) { %>
 	// if we're on touch, add the mobile template
 	if (Modernizr.touch) {
 		globe.graphicMobile($('#gf .content'), $('#gf .subtitle, #gf .source-and-credit'));
 	}
-
 	<% } %>
 	// place your code here
 
