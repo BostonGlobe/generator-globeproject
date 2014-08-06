698bbef5655031cc67c186dac0114ed0ede0c648
Gabriel Florit <gabrielflorit@gmail.com>
2014-08-06 16:39:24 -0400

    fixing broken ios7 layout

--- app/templates/js/_globe.graphic.js
+++ app/templates/js/_globe.graphic.js
@@ -3,6 +3,10 @@ globe.graphic = function() {
 	// if we're on touch, add the mobile template
 	if (Modernizr.touch) {
 		globe.graphicMobile($('#gf .content'), $('#gf .subtitle, #gf .source-and-credit'));
+
+		if (navigator.userAgent.match(/iPad;.*CPU.*OS 7_\d/i)) {
+			$('html').addClass('ipad ios7');
+		}
 	}
 	<% } %>
 	// place your code here
