28f9d0e9e129d6fbebb0f04f0b5a604467b3e1c3
Gabriel Florit <gabrielflorit@gmail.com>
2014-09-22 16:19:06 -0400

    populate .hed & .subhed from igraphic.html elements

--- graphic/templates/js/_main.js
+++ graphic/templates/js/_main.js
@@ -1,15 +1,25 @@
 (function() { globe.onDefine('window.jQuery && $(".<%= graphicType %>-graphic.<%= graphicName %>").length', function() {
-<% if (fullScreenOnMobile) { %>
+
 	var masterSelector = '.<%= graphicType %>-graphic.<%= graphicName %>';
 	var master = $(masterSelector);
+<% if (graphicType === 'igraphic') { %>
+	var hed = $('.hed', master);
+	if (hed.length) {
+		$('.header .main-hed').html(hed.html());
+	}
 
+	var subhed = $('.subhed', master);
+	if (subhed.length) {
+		$('.header .subhed').html(subhed.html());
+	}
+
+	$('.header').addClass('visible');
+<% } %><% if (fullScreenOnMobile) { %>
 	if (Modernizr.touch) {
 		globe.initMobileHeader({
 			bodyElements: $('.content', master),
 			drawerElements: $('.<%= graphicType %>-graphic.<%= graphicName %> .source-and-credit, .header .subhed')
 		});
 	}
-<% } else { %>
-	var master = $('.<%= graphicType %>-graphic.<%= graphicName %>');
 <% } %>
 }); }());
